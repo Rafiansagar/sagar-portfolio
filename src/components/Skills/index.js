459803e491dc7434ee4e2ee5d266cf85f7ea4111
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-02-13 01:09:35 +0600

    fsdfsdf

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,5 +1,16 @@
 import BoxTitle from "../Common/BoxTitle";
 
+import html from '../../img/skills/html.png';
+import css from '../../img/skills/css.png';
+import js from '../../img/skills/js.png';
+import php from '../../img/skills/php.png';
+import react from '../../img/skills/react.png';
+import wp from '../../img/skills/wp.png';
+import jmla from '../../img/skills/jmla.png';
+import sql from '../../img/skills/sql.png';
+import ps from '../../img/skills/ps.png';
+import envato from '../../img/skills/envato.png';
+
 const MySkills = (props) => {
 		const { boxClass } = props;
 	return (
@@ -10,16 +21,16 @@ const MySkills = (props) => {
 				title="About My Skill"
 			/>
 			<ul className="experiencees">
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li><img src={html} alt=""/></li>
+				<li><img src={css} alt=""/></li>
+				<li><img src={js} alt=""/></li>
+				<li><img src={php} alt=""/></li>
+				<li><img src={react} alt=""/></li>
+				<li><img src={wp} alt=""/></li>
+				<li><img src={jmla} alt=""/></li>
+				<li><img src={sql} alt=""/></li>
+				<li><img src={ps} alt=""/></li>
+				<li><img src={envato} alt=""/></li>
 			</ul>
 		</div>
 	);
