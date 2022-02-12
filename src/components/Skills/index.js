b52af94791bba69b5712f3e0c9d9d17a095b7ec4
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-02-13 00:17:52 +0600

    dfsdf

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,15 +3,24 @@ import BoxTitle from "../Common/BoxTitle";
 const MySkills = (props) => {
 		const { boxClass } = props;
 	return (
-		<div className={`about-me ${boxClass}`}>
+		<div className={`my-skills ${boxClass}`}>
 			<BoxTitle
 				sectionClass="box-title"
 				titleClass="title"
 				title="About My Skill"
 			/>
-			<p className="desc">Hi I am a Front-End developer & I can build your website as you want. I can customize plugin & wordpress theme. And I have two years of experience in figma. Now I am able to design your portfolio.</p>
-
-			<div className="desc">I have three years of experience with HTML, CSS, JavaScript. And I can Design Your web application.</div>
+			<ul className="experiencees">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
 		</div>
 	);
 }
