0e52c162201b981958cec5c2d212ef2ea28bf95a
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-02-12 19:23:42 +0600

    fsdfsdf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <React.Fragment>
+      <div></div>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
