d6c5f6bf4a1ae91990f3e1518d11c3b6b9085ad6
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-02-13 01:50:05 +0600

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import MyIntro from '../components/Intro';
 import AboutMe from '../components/About';
 import MySkills from '../components/Skills';
