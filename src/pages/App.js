56992c6fd109021294c9f65aca42e554da31f50f
Rafiansagar <35956615+Rafiansagar@users.noreply.github.com>
2022-09-10 01:23:43 +0600

    dgfg

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Circle2 } from 'react-preloaders2';
 import MyIntro from '../components/Intro';
 import AboutMe from '../components/About';
 import MySkills from '../components/Skills';
@@ -6,17 +7,26 @@ import MyServices from '../components/Services';
 import Myportfolio from '../components/Portfolio';
 
 function App() {
+	const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
 	return (
 		<React.Fragment>
-			<div className="progress-note">That's not done yet. 😁</div>
+			<Circle2 color={'#ff014f'}/>
+			<div className={isActive ? "progress-note" : 'd-none'}>
+				<div>That's not done yet. 😁</div>
+				<a className="readon" onClick={handleToggle}>Want To Watch</a>
+			</div>
 
 			<div className="main-content">
 				<div className="container">
-					<div className="row pt-100">
-						<div className="col-md-6">
+					<div className="row pt-100 xl-pt-80 md-pt-20 md-pb-20 md-ml--12 md-mr--12">
+						<div className="col-lg-6  md-pl-12 md-pr-12 md-mb-50">
 							<MyIntro />
 						</div>
-						<div className="col-md-6">
+						<div className="col-lg-6 xl-pl-30  md-pl-12 md-pr-12">
 							<AboutMe
 								boxClass="common-box mb-50"
 							/>
@@ -27,7 +37,7 @@ function App() {
 								boxClass="common-box mb-50"
 							/>
 							<Myportfolio
-								boxClass="common-box mb-50"
+								boxClass="common-box mb-50 md-mb-0"
 							/>
 						</div>
 					</div>
