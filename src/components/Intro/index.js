b4be2578f3d03ff907b689854c2b5e2f60dbe012
Rafian Sagar <sagarworld420@gmail.com>
2025-04-07 16:12:44 +0600

    Update index.js

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -11,7 +11,7 @@ function MyIntro () {
 				<h2 className="designation">Sr. Web Developer.</h2>
 				<p className="desc">
 				  <i className="ri-flashlight-line" style={{ color: 'rgb(16, 185, 129)' }}></i> Lead Developer at 
-				  <i className="ri-building-2-line" style={{ color: 'rgb(245, 11, 43)', margin: '0 0 0 6px' }}></i> RSTheme | 7+ yrs WP + Full-Stack |
+				  <i className="ri-building-2-line" style={{ color: 'rgb(245, 11, 43)', margin: '0 0 0 6px' }}></i> <a href="https://rstheme.com/" target="_blank" rel="noopener noreferrer">RSTheme</a> | 7+ yrs WP + Full-Stack |
 				  Front-end <i className="ri-gemini-line" style={{ color: 'rgb(245, 158, 11)', margin: '0 4px' }}></i> Back-end 
 				  <i className="ri-gemini-line" style={{ color: 'rgb(245, 158, 11)', margin: '0 4px' }}></i> Scalable Solutions | PHP • JS • React • HTML • CSS Wizardry
 				</p>
